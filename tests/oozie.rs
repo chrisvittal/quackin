2920c2b1b13f1e90f8963b6f1706207a1d74d6fd
z1mvader <z1mvader@protonmail.com>
2017-03-07 12:39:05 -0500

    reorganized files

---
+++ tests/oozie.rs
@@ -0,0 +1,13 @@
+extern crate oozie;
+extern crate csv;
+
+use oozie::data::{BasicDataHandler, DataHandler};
+use csv::Reader;
+
+#[test]
+fn it_works() {
+    let reader = Reader::from_file("./data/movielens.csv")
+        .unwrap().delimiter(b' ');
+    let data_handler: BasicDataHandler<usize, usize> = BasicDataHandler::new(reader);
+    println!("{:?}", data_handler.get_user_ratings(326));
+}
