9756b71eeaeaf306337c918dd906f9135e9fdf05
Christian Poveda <z1mvader@protonmail.com>
2017-05-01 09:17:21 -0500

    basicdataset only uses ratings csv file

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -14,30 +14,22 @@ pub struct BasicDataSet {
 }
 
 impl BasicDataSet {
-    pub fn from_csv(mut users_reader: Reader<File>, mut items_reader: Reader<File>, mut ratings_reader: Reader<File>) -> Self {
+    pub fn from_csv(mut ratings_reader: Reader<File>) -> Self {
         let mut user_index = 0;
         let mut item_index = 0;
         let mut user_indices = HashMap::new();
         let mut item_indices = HashMap::new();
         let mut ratings = HashMap::new();
 
-        for row in users_reader.decode() {
-            let user_id: String = row.unwrap();
-            user_indices.insert(user_id, user_index);
-            user_index += 1;
-        }
-
-        for row in items_reader.decode() {
-            let item_id: String = row.unwrap();
-            item_indices.insert(item_id, item_index);
-            item_index += 1;
-        }
-
         for row in ratings_reader.decode() {
             let (user_id, item_id, rating): (String, String, f64) = row.unwrap();
-            user_index = *user_indices.get(&user_id).expect("Invalid User ID");
-            item_index = *item_indices.get(&item_id).expect("Invalid Item ID");
+
+            user_indices.insert(user_id, user_index);
+            item_indices.insert(item_id, item_index);
             ratings.insert(user_index * item_indices.len() + item_index, rating);
+
+            user_index += 1;
+            item_index += 1;
         }
 
         Self {
