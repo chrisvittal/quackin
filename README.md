7bbb35ba0ef864ea4b4caf10aa5a8d466dfb2f1a
Christian Poveda <z1mvader@protonmail.com>
2017-05-11 02:23:37 -0500

    new name, new docs

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# Oozie
+# Quackin
 
-Oozie is a recommender systems framework written in Rust.
+Release the quackin! 🦆
+
+Quackin is a recommender systems framework written in Rust. This is a young
+project, which means two things:
+
+- There will be a lot of breaking changes until version 1.0
+- Is a perfect project for your contributions ;)
+
+Quackin was concieved as a framework for collaborative filtering recommender
+systems. But it is open to new ideas. If you want to contribute in some way
+open or start working on an issue or ping me at z1mvader@protonmail.com.
+
+One of the difficulties for developing this crate is that the Rust's machine
+learning and data mining [ecosystem](http://www.arewelearningyet.com/) is on
+an early stage, then there is no standard libraries for anything yet.
+Nevertheless, I believe that we should take advantage of the control and speed
+that Rust offers to provide a competitive alternative to frameworks written
+in languages with a more developed ecosystem like Python or Java.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+//! # Quackin - API documentation
+//!
+//! Hi, welcome to the Quackin API documentation!
+//!
+//! Quackin is a recommender systems framework written in Rust focused on:
+//!
+//! - Facilitate data handling.
+//! - Providing collaborative filtering algorithms.
+//! - Being an environment to build, test and evaluate new algorithms.
+//!
+//! Until we get a website for the crate, this docs will be the primary
+//! reference for both technical and user-side aspects of Oozie.
+//!
+//! Quackin was called Oozie as a reference to Apache Mahout, but then I
+//! discovered that Apache Oozie already exists. Now is called Quackin because
+//! I wanted a funny name and my debugger rubber duck was on my desk at that
+//! moment.
+//!
+//! ## Usage
+//! To start using Oozie just add it as a dependency to your `Cargo.toml` file:
+//!
+//! ```rust, ignore
+//! [dependencies]
+//! oozie = "*"
+//! ```
+//!
+//! ## Submodules
+//!
+//! Oozie is divided in the following submodules:
+//! - `data`: to read and handle data
+//! - `recommender`: to build recommenders
+//! - `metrics`: to measure similarities, performance, etc.
+//!
+//!
+
 extern crate csv;
 extern crate rustc_serialize;
 
